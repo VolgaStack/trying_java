23cf6d088084c99d3e9377b7162a338d9b505343
kdm <raziel.kdm@gmail.com>
2016-04-02 12:52:04 +0300

    simple calculator, javadocs, tests

---
+++ Calculator/Tests/CalculatorTest.java
@@ -0,0 +1,52 @@
+package Calculator.Tests;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Тестируем калькулятор
+ */
+public class CalculatorTest {
+    private Calculator.Calculate.Calculator calc;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        this.calc = new Calculator.Calculate.Calculator();
+    }
+
+    @org.junit.Test
+    public void add() throws Exception {
+        calc.calculate(1, 1, "+");
+        assertEquals(2, calc.getLastResult(), 0);
+
+    }
+
+    @org.junit.Test
+    public void sub() throws Exception {
+        calc.calculate(1, 1, "-");
+        assertEquals(0, calc.getLastResult(), 0);
+    }
+
+    @org.junit.Test
+    public void mult() throws Exception {
+        calc.calculate(1, 3, "*");
+        assertEquals(3, calc.getLastResult(), 0);
+    }
+
+    @org.junit.Test
+    public void div() throws Exception {
+        calc.calculate(20, 10, "/");
+        assertEquals(2, calc.getLastResult(), 0);
+    }
+
+    @org.junit.Test
+    public void divByZero() throws Exception {
+        calc.calculate(20, 0, "/");
+        assertEquals(Double.POSITIVE_INFINITY, calc.getLastResult(), 0);
+    }
+
+    @org.junit.Test
+    public void getLastResult() throws Exception {
+        Calculator.Calculate.Calculator newCalc = new Calculator.Calculate.Calculator();
+        assertEquals(0, newCalc.getLastResult(), 0);
+    }
+}
